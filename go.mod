d82ce54dc70388f0aae2db634fa76d22b876d2b0
Abin-Antu <abincantug@gmail.com>
2025-06-03 11:05:43 +0530

    urls added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Abin-Antu/browser_go
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "github.com/Abin-Antu/browser_go/browser"
+)
+
+func main() {
+	url := "https://github.com/Abin-Antu"
+    browser.OpenURL(url)
+}
